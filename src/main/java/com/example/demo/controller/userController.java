8801fc8f19d95e69867963e92ae1a30409387e59
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-10 12:29:45 +0530

    fit: completed getAllUsers end point

--- src/main/java/com/example/demo/controller/userController.java
+++ src/main/java/com/example/demo/controller/userController.java
@@ -1,16 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.userDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1")
 public class userController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/getUser")
-    public String getUser(){
-        return "user name is lahiru";
+    public List<userDTO> getUser(){
+        return userService.getAllUsers();
     }
 }
