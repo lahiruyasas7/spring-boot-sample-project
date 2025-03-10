d947e84ec565496bc14b3fd42d1f1efb79d9ccb5
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-03-10 10:54:28 +0530

    created service for user

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,29 @@
 package com.example.demo.service;
 
+
+import com.example.demo.dto.userDTO;
+import com.example.demo.model.User;
+import com.example.demo.repo.userRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    private userRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<userDTO> getAllUsers() {
+        List<User>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<userDTO>>() {}.getType());
+    }
 }
